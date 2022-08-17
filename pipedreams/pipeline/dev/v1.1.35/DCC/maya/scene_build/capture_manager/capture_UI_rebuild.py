5c23a02ad7b872753b74d9198f6c86a2afdd982d
unknown <lourou99@gmail.com>
2022-08-18 00:17:42 +0200

    progress on capture rebuild

--- pipedreams/pipeline/dev/v1.1.35/DCC/maya/scene_build/capture_manager/capture_UI_rebuild.py
+++ pipedreams/pipeline/dev/v1.1.35/DCC/maya/scene_build/capture_manager/capture_UI_rebuild.py
@@ -10,6 +10,76 @@ import maya.cmds as cmds
 def capture_UI():
 
 
+
+    def clearOptionMenu(optionMneu_path):
+        """ pass the optionmenu and it will clear all the items """
+
+        # loop through existing menus in the optionMenu and destroy them
+        for item in cmds.optionMenu(optionMneu_path, q=True, ill=True) or []:
+            cmds.deleteUI(item)
+
+
+    def versioning(path_to_dir):
+        """ This function returns the current version and the next version number """
+
+        try:
+            list_contents = os.listdir(path_to_dir)
+
+            if bool(list_contents) == False:
+                latest_version = "v001"
+
+                new_version = "v001"
+                current_version = "v001"
+                list_contents = ["v001"]
+
+            elif bool(list_contents) == True:
+                sort = sorted(list_contents, reverse=True)
+                latest_version = sort[0]
+
+                chop_name_end = latest_version[-3:]
+
+                latest_version = int(chop_name_end)
+                new_version_num = latest_version + 1
+
+                new_version = ('v{0:03d}'.format(new_version_num))
+                current_version = ('{0:03d}'.format(latest_version))
+
+                list_contents.append(new_version)
+            version_list = sorted(list_contents, reverse=True)
+
+        except FileNotFoundError:
+            new_version = ["v001"]
+            version_list = ["v001"]
+
+        return (new_version, version_list)
+
+
+
+
+    def update_version(*args):
+        """ Updates version on capture name field change update """
+
+        input_name = args[0]
+        captures_path = os.getenv("CAPTURES")
+        UI_optionMenu_path = data["UI_version_menu"]
+        UI_task_path = data["UI_task"]
+
+        Task_name = cmds.optionMenu(UI_task_path, query=True, value=True)
+
+        clearOptionMenu(UI_optionMenu_path)
+
+        path_to_capture_dir = captures_path + "/" + Task_name + "/" + input_name
+
+        for v in versioning(path_to_capture_dir)[1]:
+            cmds.menuItem(label=v, parent=UI_optionMenu_path)
+
+        # # changes the version option menu to whatever
+        # cmds.optionMenu(data["UI_version_menu"], edit=True, value="latet_version")
+
+# Build UI
+
+    data = {}
+
     if cmds.window('Capture_Manager', exists=True):
         cmds.deleteUI('Capture_Manager')
 
@@ -26,7 +96,7 @@ def capture_UI():
     cmds.textField(text=os.getenv("CAPTURES"))
 
     cmds.text(label="Task:")
-    cmds.optionMenu()
+    UI_task_path = cmds.optionMenu()
     cmds.menuItem("anim")
     cmds.menuItem("previz")
 
@@ -42,10 +112,13 @@ def capture_UI():
     cmds.optionMenu()
     cmds.menuItem("bxx_010")
     cmds.text(label="Capture Name:")
-    cmds.textField(text=os.getenv("SHOT"))
-    cmds.text(label="Capture Name:")
-    cmds.optionMenu()
-    cmds.menuItem("v001")
+    cmds.textField(text=os.getenv("SHOT"), changeCommand=update_version)
+    cmds.text(label="Version:")
+    UI_version_menu = cmds.optionMenu()
+    # cmds.menuItem("v001")
+
+
+
 
     cmds.setParent( '..' )
     cmds.separator(height=10, style='in')
@@ -84,6 +157,10 @@ def capture_UI():
     # Creates Capture
     cmds.button(label='Capture',bgc=(0.65, 1, 0))
 
+
+# Add menu path to dictionary
+    data["UI_version_menu"] = UI_version_menu
+    data["UI_task"] = UI_task_path
 
 
     cmds.showWindow(window)
