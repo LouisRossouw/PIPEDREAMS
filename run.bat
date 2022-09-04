4da832a31e618aa2f6cee8b6e25408840c26d4d5
Louis <lourou99@gmail.com>
2022-09-04 11:51:20 +0200

    .bat only runs when portable version of python is needed

---
+++ run.bat
@@ -0,0 +1,4 @@
+
+set "PATH %~dp0pipedreams\pipeline\tools\Python\Python310"
+set "PATH %~dp0pipedreams\pipeline\tools\Python\Python310\Scripts"
+start /MIN CMD.EXE /C %~dp0pipedreams\pipeline\tools\Python\Python310\python.exe  %~dp0\pipedreams\launch\run.pyw
