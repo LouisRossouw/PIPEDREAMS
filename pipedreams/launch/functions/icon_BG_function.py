b15e032b3c53e417402b70114af76765eaf8e248
unknown <lourou99@gmail.com>
2022-09-17 23:39:46 +0200

    added new day function

--- pipedreams/launch/functions/icon_BG_function.py
+++ pipedreams/launch/functions/icon_BG_function.py
@@ -1,16 +1,91 @@
-import os
-import time
+""" This file is for UI_trayIcon.pyw file - performFunction , every 10 seconds the icon will run these backroound functions """
+
+import datetime
 import requests
 
 import PySimpleGUI as sg
 from PIL import ImageGrab
+
+import utils as utils
+
+
+def performFunctions(performFunctions_count, PERFORMFUNCTION_ITERATION_COUNT, icons_dict, config, logger):
+    """ the tool will run through a list of functions that it can perform at every iteration of its while loop:
+        for example, we can add a function here that gets called that checks for any new items / assets added, and a notification system will popup,
+        Or, a function that checks email for any new emails, 
+    """
+
+    # run function IF performFunctions_count == the limit, and then reset it to 0
+    if performFunctions_count == PERFORMFUNCTION_ITERATION_COUNT:
+
+        # quick_signal = sg.SystemTray(filename=icons_dict["sig_2"])
+
+        """ add functions to run, here! """
+
+        connected_to_internet(config, icons_dict)
+        function_01(config)
+        function_02(config)
+        logger_newday(logger)
+        print("this is running")
+
+
+        # just a visual refrence to show that its performing this function
+        # quick_signal.read(timeout=100)
+        # quick_signal.update(filename=icons_dict["sig_1"])
+        # quick_signal.read(timeout=100)
+        # quick_signal.close()
+        
+        # Reset back to zero, so that the above functions will only run on every x iteration.
+        performFunctions_count = 0
+    
+    return(performFunctions_count)
+
+
+
+def logger_newday(logger):
+    """ adds a new day entry into the user_DreamLOG.log file. """
+
+    if ITS_A_NEWDAY() == True:
+        logger.info("")
+        logger.info((" * ") * 20 + (" - NEW DAY - ") + (" * ") * 20)
+        logger.info("")
+        print("newdays")
+    else:
+        pass
+
+
+
+
+def ITS_A_NEWDAY():
+    """ 
+    returns True or False if it is a new day, it returns True at midnight when the two DateTimes dont match, 
+    there is a specific time window when the two dates wont match, and other external code can perform actions during this time frame. 
+    """
+
+    time_window = 35
+    time_stamp_NOW = utils.get_time()
+    time_stamp_PAST = utils.get_time() - time_window # minus 35 seconds
+
+    datetime_NOW = datetime.datetime.fromtimestamp(time_stamp_NOW).date()
+    datetime_PAST = datetime.datetime.fromtimestamp(time_stamp_PAST).date()
+
+    # the 35 second difference will let the if statement execute at midnight when there is a
+    # difference in the current date between that 35 second window.
+    if datetime_NOW != datetime_PAST:
+        isit_a_day = True
+    else:
+        isit_a_day = False
+
+    return(isit_a_day)
+
 
 
 
 def screen_size():
     img = ImageGrab.grab()
     return (img.size)
-    
+
+
 
 
 def notification(Title, Message, icons_dict):
@@ -51,11 +126,14 @@ def connected_to_internet(config, icons_dict):
         return(connected)
 
 
+
+
 def function_01(config):
     """ function 1 wip """
 
     if config["func_1"] == True:
         pass
+
 
 
 
@@ -67,9 +145,12 @@ def function_02(config):
 
 
 
-if __name__ == "__main__":
-    config = {"func_1":True}
 
-    icons_dict = {"icon_path_1":None}
-    notification("No Internet", "No Internet Connection", icons_dict)
+if __name__ == "__main__":
+
+    # config = {"func_1":True}
+    # icons_dict = {"icon_path_1":None}
+    # notification("No Internet", "No Internet Connection", icons_dict)
+
+    ITS_A_NEWDAY()
 
