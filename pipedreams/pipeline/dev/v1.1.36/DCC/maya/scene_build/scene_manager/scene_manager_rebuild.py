824f552fb6cd1ff9d2d08b2ea8815d97251ff1f8
unknown <lourou99@gmail.com>
2022-12-10 19:58:05 +0200

    Scene Manager rebuild with PySide2 - WIP

--- pipedreams/pipeline/dev/v1.1.36/DCC/maya/scene_build/scene_manager/scene_manager_rebuild.py
+++ pipedreams/pipeline/dev/v1.1.36/DCC/maya/scene_build/scene_manager/scene_manager_rebuild.py
@@ -8,139 +8,164 @@ from PySide2 import (QtGui,
 class Scene_Manager_UI(QtWidgets.QWidget):
 
 
-        def __init__(self):
-                super().__init__()
+    def __init__(self):
+        super().__init__()
 
-                self.setWindowTitle("Scene Manager")
-                self.setMinimumSize(QtCore.QSize(1500, 400))
+        self.setWindowTitle("Scene Manager")
+        self.setMinimumSize(QtCore.QSize(1500, 400))
 
-                # Top Layout:
+    # Top Layout:
 
-                topLayout = QtWidgets.QVBoxLayout()
-                label = QtWidgets.QLabel()
-                font = QtGui.QFont()
-                Refresh_button = QtWidgets.QPushButton(text="Refresh")
+        topLayout = QtWidgets.QVBoxLayout()
+        label = QtWidgets.QLabel()
+        font = QtGui.QFont()
+        Refresh_button = QtWidgets.QPushButton(text="Refresh")
+        Refresh_button.clicked.connect(self.return_existing_categories)
 
-                font.setBold(True)
-                label.setFont(font)
-                label.setText("Go_Testify_rigging_010")
-                label.setAlignment(QtCore.Qt.AlignHCenter)
-                topLayout.addWidget(label)
-                topLayout.addWidget(Refresh_button)
+        font.setBold(True)
+        label.setFont(font)
+        label.setText("Go_Testify_rigging_010")
+        label.setAlignment(QtCore.Qt.AlignHCenter)
+        topLayout.addWidget(label)
+        topLayout.addWidget(Refresh_button)
 
-                self.line = QtWidgets.QFrame()
-                self.line.setGeometry(QtCore.QRect(0, 0, 0, 0))
-                self.line.setFrameShape(QtWidgets.QFrame.HLine)
-                self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
-                topLayout.addWidget(self.line)
-
-
-                # Tabs Layout:
-
-                Tab_group = QtWidgets.QTabWidget()
-                layout_Global_Assets = QtWidgets.QVBoxLayout()
-                layout_Top_Assets = QtWidgets.QHBoxLayout()
-                layout_Shot_Assets = QtWidgets.QHBoxLayout()
-
-                tab_1 = QtWidgets.QWidget()
-                tab_1.setLayout(layout_Global_Assets)
-
-                tab_2 = QtWidgets.QWidget()
-                tab_2.setLayout(layout_Top_Assets)
-
-                tab_3 = QtWidgets.QWidget()
-                tab_3.setLayout(layout_Shot_Assets)
-
-                Tab_group.addTab(tab_1, "Global_Assets")
-                Tab_group.addTab(tab_2, "Top_Assets")
-                Tab_group.addTab(tab_3, "Shot_Assets")
-
-                # Center Tabs.
-                self.setStyleSheet('''QTabWidget::tab-bar {alignment: center;}''')
+        self.line = QtWidgets.QFrame()
+        self.line.setGeometry(QtCore.QRect(0, 0, 0, 0))
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        topLayout.addWidget(self.line)
 
 
-                # Populate Tabs:
+    # Tabs Layout:
 
-                self.populate_Top_assets()
+        Tab_group = QtWidgets.QTabWidget()
+        self.layout_Global_Assets = QtWidgets.QVBoxLayout()
+        self.layout_Top_Assets = QtWidgets.QHBoxLayout()
+        self.layout_Shot_Assets = QtWidgets.QHBoxLayout()
 
-                for cat in ["Char", "Veh", "Env"]:
+        tab_1 = QtWidgets.QWidget()
+        tab_1.setLayout(self.layout_Global_Assets)
 
-                        self.groupBox = QtWidgets.QGroupBox(cat)
-                        layout_Global_Assets.addWidget(self.groupBox)
+        tab_2 = QtWidgets.QWidget()
+        tab_2.setLayout(self.layout_Top_Assets)
 
-                        self.groupBox.setCheckable(True)
+        tab_3 = QtWidgets.QWidget()
+        tab_3.setLayout(self.layout_Shot_Assets)
 
-                        self.v_layout = QtWidgets.QVBoxLayout()
-                        self.groupBox.setLayout(self.v_layout)
+        Tab_group.addTab(tab_1, "Global_Assets")
+        Tab_group.addTab(tab_2, "Top_Assets")
+        Tab_group.addTab(tab_3, "Shot_Assets")
 
-
-                        # Build buttons for single Asset
-                        for i in range(3):
-
-                                self.asset_layout = QtWidgets.QHBoxLayout()
-
-                                asset_Label = QtWidgets.QLabel("Hello")
-                                asset_Label.setAlignment(QtCore.Qt.AlignHCenter)
-                                asset_type = QtWidgets.QComboBox()
-                                asset_version = QtWidgets.QComboBox()
-                                asset_format = QtWidgets.QComboBox()
-
-                                Import_button = QtWidgets.QPushButton("Import")
-                                Refrence_button = QtWidgets.QPushButton("Refrence")
-                                Update_button = QtWidgets.QPushButton("Update")
-
-                                asset_user = QtWidgets.QLabel("Louis")
-                                asset_user.setAlignment(QtCore.Qt.AlignHCenter)
-
-                                self.asset_layout.addWidget(asset_Label)
-                                self.asset_layout.addWidget(asset_type)
-                                self.asset_layout.addWidget(asset_version)
-                                self.asset_layout.addWidget(asset_format)
-
-                                self.asset_layout.addWidget(Import_button)
-                                self.asset_layout.addWidget(Refrence_button)
-                                self.asset_layout.addWidget(Update_button)
-
-                                self.asset_layout.addWidget(asset_user)
-
-                                self.line = QtWidgets.QFrame()
-                                self.line.setFrameShape(QtWidgets.QFrame.HLine)
-                                self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
-                                self.v_layout.addWidget(self.line)
-
-                                self.v_layout.addLayout(self.asset_layout)
+        # Center Tabs.
+        self.setStyleSheet('''QTabWidget::tab-bar {alignment: center;}''')
 
 
+    # Populate Tabs:
 
-                # Master Layout:
+        # category = self.return_existing_categories()
 
-                master_layout = QtWidgets.QVBoxLayout()
-                master_layout.addLayout(topLayout)
-                master_layout.addWidget(Tab_group)
-                self.setLayout(master_layout)
+        TABS = ["TOP", "SHOT"]
 
-                # StyleSheet:
-                
-                self.setStyleSheet('background-color: grey;')
-                Refresh_button.setStyleSheet('background-color: grey;'
-                        'border-style: outset;'
-                        'border-width: 2px;'
-                        'border-radius: 10px;'
-                        'border-color: black;'
-                        'font: bold 14px;'
-                        'min-width: 10em;'
-                        'padding: 6px;'
-                        )
+        for tab in TABS:
+            print(tab)
+            category = self.return_existing_categories(tab)
 
 
+            for cat in category:
+
+                self.groupBox = QtWidgets.QGroupBox(cat)
+
+                self.groupBox.setCheckable(True)
+
+                self.v_layout = QtWidgets.QVBoxLayout()
+                self.groupBox.setLayout(self.v_layout)
+
+                # Populate specific category with all existing assets.
+                self.populate_assets()
+
+                if tab == "GLOBAL":
+                    self.layout_Global_Assets.addWidget(self.groupBox)
+                if tab == "TOP":
+                    self.layout_Top_Assets.addWidget(self.groupBox)
+                if tab == "SHOT":
+                    self.layout_Shot_Assets.addWidget(self.groupBox)
+
+        # Master Layout:
+
+        master_layout = QtWidgets.QVBoxLayout()
+        master_layout.addLayout(topLayout)
+        master_layout.addWidget(Tab_group)
+        self.setLayout(master_layout)
+
+        # StyleSheet:
+
+        self.setStyleSheet('background-color: grey;')
+        Refresh_button.setStyleSheet('background-color: grey;'
+                'border-style: outset;'
+                'border-width: 2px;'
+                'border-radius: 10px;'
+                'border-color: black;'
+                'font: bold 14px;'
+                'min-width: 10em;'
+                'padding: 6px;'
+                )
 
 
-        def populate_Top_assets(self):
-                """ Populates top assets """
+    def populate_assets(self):
+        """ Populates all assets for specific category. """
 
-                TOP_ASSETS = os.getenv("TOP_ASSETS")
+        # Build buttons for single Asset
+        for i in range(3):
+            self.asset_layout = QtWidgets.QHBoxLayout()
 
-                print(os.listdir(TOP_ASSETS))
+            asset_Label = QtWidgets.QLabel("Hello")
+            asset_Label.setAlignment(QtCore.Qt.AlignHCenter)
+            asset_type = QtWidgets.QComboBox()
+            asset_version = QtWidgets.QComboBox()
+            asset_format = QtWidgets.QComboBox()
+
+            Import_button = QtWidgets.QPushButton("Import")
+            Refrence_button = QtWidgets.QPushButton("Refrence")
+            Update_button = QtWidgets.QPushButton("Update")
+
+            asset_user = QtWidgets.QLabel("Louis")
+            asset_user.setAlignment(QtCore.Qt.AlignHCenter)
+
+            self.asset_layout.addWidget(asset_Label)
+            self.asset_layout.addWidget(asset_type)
+            self.asset_layout.addWidget(asset_version)
+            self.asset_layout.addWidget(asset_format)
+
+            self.asset_layout.addWidget(Import_button)
+            self.asset_layout.addWidget(Refrence_button)
+            self.asset_layout.addWidget(Update_button)
+
+            self.asset_layout.addWidget(asset_user)
+
+            self.line = QtWidgets.QFrame()
+            self.line.setFrameShape(QtWidgets.QFrame.HLine)
+            self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+            self.v_layout.addWidget(self.line)
+
+            self.v_layout.addLayout(self.asset_layout)
+
+
+
+    def return_existing_categories(self, tab):
+        """ Returns the existing ca """
+
+        asset_tab = tab + "_ASSETS"
+
+        existing_ASSETS = os.getenv(asset_tab)
+        existing_categories = os.listdir(existing_ASSETS)
+
+        return existing_categories
+
+
+    def return_assets(path, category):
+        """ Returns the list of assets for the specific category. """
+
+        print(path, category)
 
 
 
@@ -148,7 +173,8 @@ class Scene_Manager_UI(QtWidgets.QWidget):
 
 
 
-# app = QtWidgets.QApplication()
+
+#app = QtWidgets.QApplication()
 Scene_Manager_UI = Scene_Manager_UI()
 Scene_Manager_UI.show()
-# app.exec_()
+#app.exec_()
