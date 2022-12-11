f448b6ce2572cbe1c304d6e98f05ffe2750ef625
Louis <lourou99@gmail.com>
2022-12-11 13:55:18 +0200

    Scene Managr rebuild with PySide2 - WIP.

--- pipedreams/pipeline/dev/v1.1.36/DCC/maya/scene_build/scene_manager/scene_manager_rebuild.py
+++ pipedreams/pipeline/dev/v1.1.36/DCC/maya/scene_build/scene_manager/scene_manager_rebuild.py
@@ -1,4 +1,5 @@
 import os
+from functools import partial
 from PySide2 import (QtGui,
                     QtCore,
                     QtWidgets)
@@ -6,7 +7,7 @@ from PySide2 import (QtGui,
 
 
 class Scene_Manager_UI(QtWidgets.QWidget):
-
+    """ Builds the Scene Manager UI for the importing of assets. """
 
     def __init__(self):
         super().__init__()
@@ -64,6 +65,7 @@ class Scene_Manager_UI(QtWidgets.QWidget):
     # Populate Tabs:
 
         TABS = ["TOP", "SHOT"]
+        self.asset_dict = {}
 
         for tab in TABS:
 
@@ -78,7 +80,7 @@ class Scene_Manager_UI(QtWidgets.QWidget):
                 self.groupBox.setLayout(self.v_layout)
 
                 # Populate specific category with all existing assets.
-                self.populate_assets(cat, category[1])
+                self.populate_assets(cat, category[1], tab)
 
                 if tab == "GLOBAL":
                     self.layout_Global_Assets.addWidget(self.groupBox)
@@ -110,52 +112,82 @@ class Scene_Manager_UI(QtWidgets.QWidget):
                 )
 
 
-    def populate_assets(self, category, path):
+
+
+    def populate_assets(self, category, path, tab):
         """ Populates all assets for specific category. """
 
         category_path = path + '/' + category
         category_dir = os.listdir(category_path)
 
         # Build buttons for single Asset
+        row_index = 0
+
         for i in category_dir:
+
+            row_index += 1
+            unique_name = tab + "_" + category + "_" + str(i) + "_" + str(row_index)
 
             self.asset_layout = QtWidgets.QHBoxLayout()
 
-            asset_Label = QtWidgets.QLabel(i)
-            asset_Label.setAlignment(QtCore.Qt.AlignHCenter)
-            asset_type = QtWidgets.QComboBox()
-            asset_version = QtWidgets.QComboBox()
-            asset_format = QtWidgets.QComboBox()
+            self.asset_Label = QtWidgets.QLabel(i)
+            self.asset_Label.setAlignment(QtCore.Qt.AlignHCenter)
+            self.asset_type = QtWidgets.QComboBox()
+            self.asset_version = QtWidgets.QComboBox()
+            self.asset_format = QtWidgets.QComboBox()
 
-            Import_button = QtWidgets.QPushButton("Import")
-            Refrence_button = QtWidgets.QPushButton("Refrence")
-            Update_button = QtWidgets.QPushButton("Update")
+            self.Import_button = QtWidgets.QPushButton("Import", clicked=partial(self.button_import, unique_name))
+            self.Refrence_button = QtWidgets.QPushButton("Refrence")
+            self.Update_button = QtWidgets.QPushButton("Update")
 
             asset_user = QtWidgets.QLabel("Louis")
             asset_user.setAlignment(QtCore.Qt.AlignHCenter)
 
-            self.asset_layout.addWidget(asset_Label)
-            self.asset_layout.addWidget(asset_type)
-            self.asset_layout.addWidget(asset_version)
-            self.asset_layout.addWidget(asset_format)
+            self.asset_layout.addWidget(self.asset_Label)
+            self.asset_layout.addWidget(self.asset_type)
+            self.asset_layout.addWidget(self.asset_version)
+            self.asset_layout.addWidget(self.asset_format)
 
-            self.asset_layout.addWidget(Import_button)
-            self.asset_layout.addWidget(Refrence_button)
-            self.asset_layout.addWidget(Update_button)
+            self.asset_layout.addWidget(self.Import_button)
+            self.asset_layout.addWidget(self.Refrence_button)
+            self.asset_layout.addWidget(self.Update_button)
 
             self.asset_layout.addWidget(asset_user)
+
+            self.asset_dict[unique_name] = {"asset_name": i,
+                                            "asset_label": self.asset_Label,
+                                            "asset_type": self.asset_type,
+                                            "asset_version": self.asset_version,
+                                            "asset_format": self.asset_format,
+                                            "asset_path": category_path
+                                           }
+
 
             # add data to the comboBox / optionMenus
             asset_data = self.return_assets(category_path + "/" + i)
 
-            asset_type.addItems(asset_data[0])
-            asset_version.addItems(asset_data[1])
-            asset_format.addItems(asset_data[2])
-
-
+            self.asset_type.addItems(asset_data[0])
+            self.asset_version.addItems(asset_data[1])
+            self.asset_format.addItems(asset_data[2])
 
             # add to layout.
             self.v_layout.addLayout(self.asset_layout)
+
+
+
+
+    def button_import(self, row_index):
+        """ Function for import button when cllicked. """
+
+        # Returns asset info from row.
+        asset_name = self.asset_dict[row_index]["asset_name"]
+        asset_type = self.asset_dict[row_index]["asset_type"].currentText()
+        asset_version = self.asset_dict[row_index]["asset_version"].currentText()
+        asset_format = self.asset_dict[row_index]["asset_format"].currentText()
+        asset_path = self.asset_dict[row_index]["asset_path"]
+
+        print(asset_name, asset_type, asset_version, asset_format, asset_path)
+
 
 
 
@@ -168,6 +200,8 @@ class Scene_Manager_UI(QtWidgets.QWidget):
         existing_categories = os.listdir(existing_ASSETS_path)
 
         return existing_categories, existing_ASSETS_path
+
+
 
 
     def return_assets(path, asset_path):
@@ -185,6 +219,8 @@ class Scene_Manager_UI(QtWidgets.QWidget):
             formats.append(format)
 
         return asset_types, reversed(versions), formats
+
+
 
 
     def reorder(self, asset_type):
